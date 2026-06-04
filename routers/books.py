8f9f005718805831435dd2b710e1a5cd04abb0c0
VLEAIT <133334975+VLEAIT@users.noreply.github.com>
2026-06-04 02:59:57 +0000

    fix:made the pipleine in routes more proffesional and auto

--- routers/books.py
+++ routers/books.py
@@ -1,8 +1,10 @@
-from fastapi import HTTPException,APIRouter,Depends
+from fastapi import HTTPException,APIRouter,Depends,status
 from sqlalchemy.orm import Session
 from database import get_db
-from models import Book
+from models import Book,User
 from schemas import BookCreate,BookResponse,BookUpdate
+from typing import Annotated
+from core import get_current_user
 
 
 router=APIRouter(
@@ -10,9 +12,23 @@ router=APIRouter(
     tags=["books"]
 )
 
+DatabaseSession=Annotated[Session, Depends(get_db)]
+CurrentUser=Annotated[User,Depends(get_current_User)]
+
+def get_book_byid(db:DatabaseSession)->Book:
+        book=db.query(Book).filter(Book.id==id).first()
+        if not book:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="data not found"
+            )
+        return book    
+
+Target=Annotated[Book,Depends(get_book_byid)]   
+
 @router.post("/",response_model=BookResponse,status_code=201)
-def book_create(book:BookCreate,db:Session=Depends(get_db)):
-    db_book=Book(**book.model_dump())
+def book_create(book:BookCreate,db:DatabaseSession,current_user:CurrentUser):
+    db_book=Book(**book.model_dump(),owner_id=current_user.id)
     db.add(db_book)
     db.commit()
     db.refresh(db_book)
@@ -20,45 +36,38 @@ def book_create(book:BookCreate,db:Session=Depends(get_db)):
 
 
 @router.get("/",response_model=list[BookResponse])
-def get_bookall(point_skip:int=0,limit:int=10,db:Session=Depends(get_db)):
+def get_bookall(db:DatabaseSession,point_skip:int=0,limit:int=10):
     return db.query(Book).offset(point_skip).limit(limit).all()
     
 
 @router.get("/{id}",response_model=BookResponse)
-def get_book(id:int,db:Session=Depends(get_db)):
-    book=db.query(Book).filter(Book.id==id).first()
-    if not book: 
-        raise HTTPSException(status_code=404,detail="Book Not Found") 
+def get_book(id:int,book:Target,db:DatabaseSession):
     return book
+  
 
-@router.put("/{id}",reponse_model=BookResponse)
-def put_book(id:int,book:BookCreate,db:Session=Depends(get_db)):
-    db_book=db.query(Book).filter(Book.id==id).first()
-    if not db_book:
-        HTTPSException(status_code=404,detail="Book not found")
-
+@router.put("/{id}",response_model=BookResponse)
+def put_book(book:BookCreate,db:DatabaseSession,create_user:CurrentUser,db_book:Target):
+    if db_book.owner_id!=create_user.id:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="not authorized")
     for key,value in book.model_dump().items():
         setattr(db_book,key,value)
+    db_book.owner_id=create_user.id    
     db.commit()
     db.refresh(db_book)
     return db_book
 
 @router.patch("/{id}",response_model=BookResponse)
-def patch_book(id:int,book:BookUpdate,db:Session=Depends(get_db)):
-    db_book=db.query(Book).filter(Book.id==id).first()
-    if not db_book:
-        raise HTTPException(status_code=404,detail="Book not found")
+def patch_book(book:BookUpdate,db:DatabaseSession,create_user:CurrentUser,db_book:Target):
+    if db_book.owner_id!=create_user.id:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="not authorized")
     for key, value in book.model_dump(exclude_unset=True).items():
         setattr(db_book,key,value)
     db.commit()
     db.refresh(db_book)
-    return(db_book)
+    return db_book
 
 @router.delete("/{id}",status_code=status.HTTP_204_NO_CONTENT)
-def delete(id:int,current_user:User=Depends(get_cuurent_user),db:Session=Depends(get_db)):
-    db_book=db.query(Book).filter(Book.id==id).first()
-    if not db_book:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="no data found")
+def delete(db:DatabaseSession,current_user:CurrentUser,db_book:target):
     if db_book.owner_id != current_user.id:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail=" user is not authroried")
     db.delete(db_book)
